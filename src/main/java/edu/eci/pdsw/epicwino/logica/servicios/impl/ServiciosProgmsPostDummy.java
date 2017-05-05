5f837fe4fbbe8ce90524feed7393787056b58104
Maria Camila Gomez Ramirez <maria.gomez-ra@mail.escuelaing.edu.co>
2017-05-05 01:11:09 -0500

    Poblada del dummy del metodo poblar, para programas, asignaturas y materias

--- src/main/java/edu/eci/pdsw/epicwino/logica/servicios/impl/ServiciosProgmsPostDummy.java
+++ src/main/java/edu/eci/pdsw/epicwino/logica/servicios/impl/ServiciosProgmsPostDummy.java
@@ -31,7 +31,76 @@ public final class ServiciosProgmsPostDummy implements ServiciosProgmsPost {
     }
     
     public void poblar() {
-        // TODO implementar
+        Asignatura a;
+        Materia m;
+        
+        Programa p1=new Programa(0,"Especializacion en gerencia de proyectos");
+        Programa p2=new Programa(1,"Maestria en gestion de informacion");
+        
+        List<Asignatura> asignaturas1=new ArrayList<>();
+        List<Asignatura> asignaturas2=new ArrayList<>();
+        
+        List<Materia> materias11=new ArrayList<>();
+        List<Materia> materias12=new ArrayList<>();
+        List<Materia> materias21=new ArrayList<>();
+        List<Materia> materias22=new ArrayList<>();
+        
+        a=new Asignatura(0,"Fundamentos de gerencia de proyectos");
+        
+        m=new Materia(0,"Adminitrar un proyecto");
+        materias11.add(m);
+        m=new Materia(1,"Fundamentos de gerencia");
+        materias11.add(m);
+        m=new Materia(2,"Tipos de proyectos");
+        materias11.add(m);
+        
+        a.setMaterias(materias11);
+        asignaturas1.add(a);
+        
+        a=new Asignatura(1,"Teorias gerenciales");
+        
+        m=new Materia(3,"Teoria de la gerencia");
+        materias12.add(m);
+        m=new Materia(4,"Teoria de la organizacion");
+        materias12.add(m);
+        m=new Materia(5,"Teoria administrativa");
+        materias12.add(m);
+        
+        a.setMaterias(materias12);
+        asignaturas1.add(a);
+        
+        p1.setAsignaturas(asignaturas1);
+        
+        a=new Asignatura(2,"Fundamentos de la informacion");
+        
+        m=new Materia(6,"Organizar informacion");
+        materias21.add(m);
+        m=new Materia(7,"Informacion en proyectos");
+        materias21.add(m);
+        m=new Materia(8,"Teorias de la informacion");
+        materias21.add(m);
+        
+        a.setMaterias(materias21);
+        asignaturas2.add(a);
+        
+        a=new Asignatura(3,"Organizacion gerencial");
+        asignaturas2.add(a);
+        
+        m=new Materia(9,"Teorias de la Organizacion");
+        materias22.add(m);
+        m=new Materia(10,"La gerencia y las organizaciones");
+        materias22.add(m);
+        m=new Materia(11,"Administracion organizacional");
+        materias22.add(m);
+        
+        a.setMaterias(materias22);
+        asignaturas2.add(a);
+       
+        p2.setAsignaturas(asignaturas2);
+        
+        programas.add(p1);
+        programas.add(p2);
+     
     }
 
     @Override
