b3d6634334b26e42842fb14a8e55cbaf5ac5eaba
Esteban Murcia <Esteban Murcia>
2017-05-22 16:12:57 -0500

    Implementacion Casos de Prueba
    Coberturas Agregadas:
    - Programas (Necesita Cambios)
    - Clases (consultarClasesPorPeriodo)

--- src/test/java/edu/eci/pdsw/epicwino/tests/ProgramasTest.java
+++ src/test/java/edu/eci/pdsw/epicwino/tests/ProgramasTest.java
@@ -5,15 +5,25 @@
  */
 package edu.eci.pdsw.epicwino.tests;
 
+import edu.eci.pdsw.epicwino.logica.entidades.Asignatura;
+import edu.eci.pdsw.epicwino.logica.entidades.Clase;
+import edu.eci.pdsw.epicwino.logica.entidades.Materia;
+import edu.eci.pdsw.epicwino.logica.entidades.Programa;
+import edu.eci.pdsw.epicwino.logica.servicios.ExcepcionServiciosProgmsPost;
+import edu.eci.pdsw.epicwino.logica.servicios.ServiciosProgmsPost;
+import edu.eci.pdsw.epicwino.logica.servicios.ServiciosProgmsPostFactory;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -23,11 +33,13 @@ import org.junit.Test;
  * 
  * Clases de Equivalencia
  * CE1: registrar adecuadamente varios programas y consultarlos para un periodo (2)
- * CE2: registrar adecuadamente un programa y consultarlo para asignaturas que tiene inscritas (1)
+ * CE2: registrar adecuadamente un programa y consultarlo en base a una asignatura (1)
+ * CE3: registrar adecuadamente varios programsa y consultarlos en base a una asignatura que tienen inscritas; asignatura perteneciente a dos programas (1)
  * 
  * Clases de Frontera
  * CF1: no registrar un programa que ya existie (1)
  * CF2: no consultar un programa para un periodo negativo, registrado
+ * CF3: no consultar un programa asociado a una asignatura que no existe
  */
 public class ProgramasTest {
 
@@ -37,19 +49,114 @@ public class ProgramasTest {
     @BeforeClass
     public static void setUp() {
     }
+    
+    @Test
+    public void CE1() throws ExcepcionServiciosProgmsPost{
+        ServiciosProgmsPost sp = ServiciosProgmsPostFactory.getInstance().getServiciosProgmsPostTesting();
+        Programa prg1 = new Programa(20,"Gerencia de Proyectos", "Especializacion");
+        Programa prg2 = new Programa(21,"Gestion de Informacion", "Maestria");
+        Asignatura as1 = new Asignatura(30,"Ejecucion");
+        Asignatura as2 = new Asignatura(31,"Operacion");
+        Materia m1 = new Materia(50,"Gerencia Financiera");
+        Materia m2 = new Materia(51,"Analisis de Riesgos");
+        Clase cl1 = new Clase(40,java.sql.Date.valueOf("2015-04-08"),java.sql.Time.valueOf("07:00:00"),java.sql.Time.valueOf("10:00:00"));
+        Clase cl2 = new Clase(41,java.sql.Date.valueOf("2015-04-13"),java.sql.Time.valueOf("08:00:00"),java.sql.Time.valueOf("11:00:00"));
+        
+        sp.registrarPrograma(prg1);
+        sp.registrarPrograma(prg2);
+        sp.registrarAsignatura(as1, 20);
+        sp.registrarAsignatura(as2, 21);
+        sp.registrarMateria(m1, 30);
+        sp.registrarMateria(m2, 31);
+        sp.agregarClase(50, cl1);
+        sp.agregarClase(51, cl2);
+        
+        Collection<Programa> progPorPer = sp.consultarProgramas(20151);
+        assertEquals("Se registra o consulta inadecuadamente los programas programados para un periodo academico"
+                    + "cuando esta se debe mostrar : "
+                    ,2,progPorPer.size());
+    }
     /*
     @Test
-    public void CE1(){}
+    public void CE2() throws ExcepcionServiciosProgmsPost{
+        ServiciosProgmsPost sp = ServiciosProgmsPostFactory.getInstance().getServiciosProgmsPostTesting();
+        Programa prg1 = new Programa(22,"Gerencia de Proyectos", "Especializacion");
+        Asignatura as1 = new Asignatura(32,"Ejecucion");
+        
+        sp.registrarPrograma(prg1);
+        sp.registrarAsignatura(as1, 22);
+        
+        Collection<Programa> progPorAsignatura = sp.consultarPrograma(32);
+        assertEquals("Se registra o consulta inadecuadamente el programa asociado a una asignatura"
+                    + "cuando esta se debe mostrar : "
+                    ,1,progPorAsignatura.size());
+    }
     
     @Test
-    public void CE2(){}
+    public void CE3() throws ExcepcionServiciosProgmsPost{
+        ServiciosProgmsPost sp = ServiciosProgmsPostFactory.getInstance().getServiciosProgmsPostTesting();
+        Programa prg1 = new Programa(23,"Gerencia de Proyectos", "Especializacion");
+        Programa prg2 = new Programa(24,"Gestion de Informacion", "Maestria");
+        Asignatura as1 = new Asignatura(33,"Ejecucion");
+        
+        sp.registrarPrograma(prg1);
+        sp.registrarPrograma(prg2);
+        sp.registrarAsignatura(as1, 23);
+        sp.registrarAsignatura(as1, 24);
+        
+        Collection<Programa> progPorAsignatura = sp.consultarPrograma(33);
+        assertEquals("Se registra o consulta inadecuadamente los programas asociados a una asignatura"
+                    + "cuando esta se debe mostrar : "
+                    ,2,progPorAsignatura.size());
+    }*/
     
     @Test
-    public void CF1(){}
+    public void CF1(){
+        ServiciosProgmsPost sp = ServiciosProgmsPostFactory.getInstance().getServiciosProgmsPostTesting();
+        Programa prg1 = new Programa(25,"Gerencia de Proyectos", "Especializacion");
+        boolean thrown = false;
+        try {
+            sp.registrarPrograma(prg1);
+            sp.registrarPrograma(prg1);
+        } catch (ExcepcionServiciosProgmsPost ex) {
+            thrown = true;
+        }
+        assertTrue("Se registra inadecuadamente un programa que ya existe"
+                + ", cuando esta debe lanzar ExcepcionServiciosProgmsPost",thrown); 
+    }
     
     @Test
-    public void CF2(){}
-    */
+    public void CF2(){
+        ServiciosProgmsPost sp = ServiciosProgmsPostFactory.getInstance().getServiciosProgmsPostTesting();
+        Programa prg1 = new Programa(26,"Gerencia de Proyectos", "Especializacion");
+        boolean thrown = false;
+        try {
+            sp.registrarPrograma(prg1);
+            Collection<Programa> progPorPer = sp.consultarProgramas(-20151);
+        } catch (ExcepcionServiciosProgmsPost ex) {
+            thrown = true;
+        }
+        assertTrue("Se consulta inadecuadamente un programa para un periodo inconsistente"
+                + ", cuando esta debe lanzar ExcepcionServiciosProgmsPost",thrown); 
+    }
+    
+    @Test
+    public void CF3(){
+        ServiciosProgmsPost sp = ServiciosProgmsPostFactory.getInstance().getServiciosProgmsPostTesting();
+        Programa prg1 = new Programa(27,"Gerencia de Proyectos", "Especializacion");
+        Asignatura as1 = new Asignatura(34,"Ejecucion");
+        boolean thrown = false;
+        try {
+            sp.registrarPrograma(prg1);
+            sp.registrarAsignatura(as1, 27);
+            sp.consultarPrograma(100);
+        } catch (ExcepcionServiciosProgmsPost ex) {
+            thrown = true;
+        }
+        assertTrue("Se consulta inadecuadamente un programa consultado con una asignatura que no existe"
+                + ", cuando esta debe lanzar ExcepcionServiciosProgmsPost",thrown); 
+    }
+    
     @AfterClass
     public static void tearDown() {
         JdbcDataSource ds= new JdbcDataSource();
